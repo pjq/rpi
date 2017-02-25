14c1902f1360b54861a7eedcab27e730fa992125
jianqing <pengjianqing@gmail.com>
2017-02-25 23:09:29 +0800

    add the rotate from 0-90-180-90-0 degree only one time

---
+++ sg90_micro_serve/sg90_1time.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+
+SERVO = 14
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO, GPIO.OUT)
+p = GPIO.PWM(SERVO, 50)
+p.start(5)
+p.ChangeDutyCycle(5)
+time.sleep(1)
+p.ChangeDutyCycle(7.5)
+time.sleep(1)
+p.ChangeDutyCycle(10)
+time.sleep(1)
+p.ChangeDutyCycle(7.5)
+time.sleep(1)
+p.ChangeDutyCycle(5)
+time.sleep(1)
+print "Done loop"
+p.stop()
+GPIO.cleanup()
