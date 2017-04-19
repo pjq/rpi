c34f64f8c3af2de8f2e7cc85ba9ac4cd73b088d5
Jianqing Peng <pengjianqing@gmail.com>
2017-04-19 22:47:38 +0800

    add ssh_forward.sh

---
+++ tools/ssh_forward.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# = 2 ];then
+    localport=$1
+    remoteport=$2
+    echo ssh -gNfR ef.pjq.me:${remoteport}:localhost:${localport} pjq@ef.pjq.me
+    ssh -gNfR ef.pjq.me:${remoteport}:localhost:${localport} pjq@ef.pjq.me
+    echo DONE, Now you can visit it via
+    echo http://ef.pjq.me:${remoteport}
+else
+cat <<EOF
+    Usage: 
+    ./$0 localport remoteport
+    For example, forward the localhost port 80 to the remote server 8080
+    ssh -gNfR ef.pjq.me:8080:localhost:80 pjq@ef.pjq.me
+    Then, you can visit it via: http://ef.pjq.me:8080
+EOF
+
+fi
+
+
