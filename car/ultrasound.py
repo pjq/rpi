6717795c544f9c985e020fb1ec04cb559f3464d7
Jianqing Peng <pengjianqing@gmail.com>
2017-07-31 23:54:53 +0800

    fix the ultrasound.py

--- car/ultrasound.py
+++ car/ultrasound.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import time
 import RPi.GPIO as GPIO
@@ -16,18 +16,18 @@ GPIO.setup(PIN2, GPIO.IN)
 GPIO.output(PIN1, GPIO.LOW)
  
 while  1:
-   time.sleep(1)
+   time.sleep(0.5)
    # IO 触发，给10us的高电平
    # 模块自动发送8个40khz的方波
    GPIO.output(PIN1, GPIO.HIGH)
    time.sleep(0.000010)
    GPIO.output(PIN1, GPIO.LOW)
    # 获取发射完毕时间
-   t1 = time.time()
    # 未接收为False，循环检查开始接受点
    # 转为True为开始接受
    while GPIO.input(PIN2) == False:
        pass
+   t1 = time.time()
    # 循环检查开始接收，转为False。则为接受完毕
    while GPIO.input(PIN2):
        pass
