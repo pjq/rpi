963d648236367d8f55b6cc6d7205f867101b550c
Jianqing Peng <pengjianqing@gmail.com>
2017-08-13 12:03:39 +0800

    update auto drive script

---
+++ car/kill.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#echo kill `ps aux|grep auto_car.py|awk -F "  " '{print $3}'|grep -v "^$"`
+list=`ps aux|grep auto_car.py|grep -v "grep"|awk -F "  " '{print $3}'`
+echo ${list}
+kill ${list}
+list=`ps aux|grep auto_car.py|grep -v "grep"|awk -F "  " '{print $5}'`
+echo ${list}
+kill ${list}
--- car/stop.py
+++ car/stop.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python 
 import time
 import sys
+import os
 import controller
 
 if __name__=="__main__":
-    sleep=5
+    sleep=1
     if len(sys.argv)==2:
         sleep=float(sys.argv[1])
     try:
+        cmd="sh /home/pi/rpi/car/kill.sh"
+        print cmd
+        os.system(cmd)
         controller.stop(sleep)
     except KeyboardInterrupt:
         print "KeyboardInterrupt"
