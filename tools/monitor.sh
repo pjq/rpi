5e59796eaf9b1f91d8dc88b534b8aa0cb3002cda
Jianqing Peng <pengjianqing@gmail.com>
2018-10-05 21:03:15 +0800

    update the monitor script

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo `date` "start monitor pm2.5..."
+echo `date` "start rpi car monitor..."
 
-sleep_seconds=600
+sleep_seconds=120
 i=0
 while true;
 do
     echo `date` "loop times "${i}
-    sh capture_image.sh
+    #sh capture_image.sh
     sleep ${sleep_seconds} 
-    sh autoforward.sh
+    sh autoforward.sh&
     i=$((i+1))
 done
 
